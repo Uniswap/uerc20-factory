c564f1ca4bf74accd19925daa5e9b9e22aa9cff4
Alice Henshaw <henshawalice@gmail.com>
2025-02-12 12:15:38 +0000

    Add basic gas tests

--- test/TokenFactory.t.sol
+++ test/TokenFactory.t.sol
@@ -15,11 +15,25 @@ contract TokenFactoryTest is Test {
     function test_create_succeeds() public {
         string memory name = "Test Token";
         string memory symbol = "TOKEN";
+
         Token tokenAddress = factory.create(name, symbol);
+        vm.snapshotGasLastCall("deploy new token");
 
         assert(address(tokenAddress) != address(0));
         assertEq(tokenAddress.name(), name);
         assertEq(tokenAddress.symbol(), symbol);
         assertEq(tokenAddress.decimals(), 18);
+    }
+
+    function test_bytecodeSize_factory() public {
+        vm.snapshotValue("TokenFactory bytecode size", address(factory).code.length);
+    }
+
+    function test_bytecodeSize_token() public {
+        string memory name = "Test Token";
+        string memory symbol = "TOKEN";
+
+        Token tokenAddress = factory.create(name, symbol);
+        vm.snapshotValue("Token bytecode size", address(tokenAddress).code.length);
     }
 }
