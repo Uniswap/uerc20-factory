15188f5b666df5387c6e900d12c84caa65f319e4
dianakocsis <diana.kocsis@uniswap.org>
2025-02-25 15:31:59 -0500

    creator first

--- src/libraries/TokenMetadata.sol
+++ src/libraries/TokenMetadata.sol
@@ -5,32 +5,32 @@ import {Base64} from "openzeppelin-contracts/contracts/utils/Base64.sol";
 import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 struct TokenMetadata {
+    address creator;
     string description;
     string website;
     string image;
-    address creator;
 }
 
 library TokenMetadataLibrary {
     using Strings for address;
 
     function toJSON(TokenMetadata memory metadata) public pure returns (string memory) {
-        return string(abi.encodePacked("data:application/json;base64,", Base64.encode(bytes(formatJSON(metadata)))));
+        return string(abi.encodePacked("data:application/json;base64,", Base64.encode(displayMetadata(metadata))));
     }
 
-    function formatJSON(TokenMetadata memory metadata) private pure returns (string memory) {
-        string memory json = string.concat('{"Creator":"', metadata.creator.toChecksumHexString(), '"');
+    function displayMetadata(TokenMetadata memory metadata) private pure returns (bytes memory) {
+        bytes memory json = abi.encodePacked('{"Creator":"', metadata.creator.toChecksumHexString(), '"');
 
         if (bytes(metadata.description).length > 0) {
-            json = string.concat(json, ', "Description":"', metadata.description, '"');
+            json = abi.encodePacked(json, ', "Description":"', metadata.description, '"');
         }
         if (bytes(metadata.website).length > 0) {
-            json = string.concat(json, ', "Website":"', metadata.website, '"');
+            json = abi.encodePacked(json, ', "Website":"', metadata.website, '"');
         }
         if (bytes(metadata.image).length > 0) {
-            json = string.concat(json, ', "Image":"', metadata.image, '"');
+            json = abi.encodePacked(json, ', "Image":"', metadata.image, '"');
         }
 
-        return string.concat(json, "}");
+        return abi.encodePacked(json, "}");
     }
 }
