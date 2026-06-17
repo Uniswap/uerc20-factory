bf23213ab1b64452cdec2f361915941f1450704e
Eric Zhong <eric.zhong@uniswap.org>
2026-06-17 08:24:14 -0400

    nits

--- src/libraries/UERC20MetadataLibrary.sol
+++ src/libraries/UERC20MetadataLibrary.sol
@@ -8,8 +8,8 @@ struct UERC20Metadata {
     string description;
     string website;
     string image;
-    /// @dev Opaque, application-defined data emitted on token creation and stored on-chain.
-    /// Not included in the rendered tokenURI JSON; intended for off-chain verification.
+    /// @dev Arbitrary application-defined data emitted on token creation and stored on-chain.
+    /// Not included in the rendered tokenURI JSON, can be fetched by `token.metadata()`.
     bytes extraData;
 }
 
